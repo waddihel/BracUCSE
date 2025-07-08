dd30066f90a4b9ca0724a0cdaeaf5075d42e6141
Md. Farhadul Kabir <74975531+waddihel@users.noreply.github.com>
2025-07-08 23:17:21 +0600

    Update HexagonArea.java
    fixed the circumference formula

--- CSE110/lab/Assignment-1/homeTasks/HexagonArea.java
+++ CSE110/lab/Assignment-1/homeTasks/HexagonArea.java
@@ -7,7 +7,7 @@ public class HexagonArea{
 
       double area = (3*Math.sqrt(3)*side*side)/2;
 
-      double circumference = side*5;
+      double circumference = side*6;
 
       System.out.println("The area of the hexagon is: " + area);
       System.out.println("The circumference of the hexagon is: " + circumference);
